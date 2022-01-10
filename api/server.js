61a6cc4ef5f5e32d8efb4040e8a7da525af309ee
wb1110 <strengthcoachwb@gmail.com>
2022-01-10 13:49:12 -0600

    put complete

--- api/server.js
+++ api/server.js
@@ -5,16 +5,17 @@ server.use(express.json()) //parse json from requests
 const User = require('./users/model')
 
 server.post('/api/users', (req, res) => {
-
+    const { name, bio } = req.body
+    User.insert({ name, bio })
+    .then(newUser => {
+        res.status(201).json(newUser)
+    })
+   .catch(err => {
+        res.status(500).json({ message: err.message })
+    })
 })
 
 server.get('/api/users', async (req, res) => {
-    // try {
-    //     const users = await User.find()
-    //     res.json(users)
-    // } catch(err) {
-    //     res.status(500).json({ massage: err.message })
-    // }
     User.find()
         .then(users => {
             res.json(users)
@@ -28,19 +29,33 @@ server.get('/api/users/:id', (req, res) => {
     const { id } = req.params
     User.findById(id)
     .then(user => {
-        res.json(user)
+        user ? res.status(200).json(user) : res.status(404).json({ message: "no user" })
     })
    .catch(err => {
         res.status(500).json({ message: err.message })
-})
+    })
 })
 
 server.delete('/api/users/:id', (req, res) => {
-    
+    const { id } = req.params
+    User.remove(id)
+    .then(user => {
+        user ? res.status(200).json(user) : res.status(404).json({ message: "no user" })
+    })
+   .catch(err => {
+        res.status(500).json({ message: err.message })
+    })
 })
 
 server.put('/api/users/:id', (req, res) => {
-    
+    const { id } = req.params
+    User.update(id, changes)
+    .then(user => {
+        user ? res.status(200).json(user) : res.status(404).json({ message: "no user" })
+    })
+   .catch(err => {
+        res.status(500).json({ message: err.message })
+    })
 })
 
-module.exports = server; // EXPORT YOUR SERVER instead of {}
+module.exports = server;
