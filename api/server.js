383b7426f4ea8929f6612e15a576c2cd16ffba32
wb1110 <strengthcoachwb@gmail.com>
2022-01-10 13:26:32 -0600

    users and user done

--- api/server.js
+++ api/server.js
@@ -1,3 +1,46 @@
 // BUILD YOUR SERVER HERE
+const express = require('express')
+const server = express()
+server.use(express.json()) //parse json from requests
+const User = require('./users/model')
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+server.post('/api/users', (req, res) => {
+
+})
+
+server.get('/api/users', async (req, res) => {
+    // try {
+    //     const users = await User.find()
+    //     res.json(users)
+    // } catch(err) {
+    //     res.status(500).json({ massage: err.message })
+    // }
+    User.find()
+        .then(users => {
+            res.json(users)
+        })
+       .catch(err => {
+            res.status(500).json({ message: err.message })
+    })
+})
+
+server.get('/api/users/:id', (req, res) => {
+    const { id } = req.params
+    User.findById(id)
+    .then(user => {
+        res.json(user)
+    })
+   .catch(err => {
+        res.status(500).json({ message: err.message })
+})
+})
+
+server.delete('/api/users/:id', (req, res) => {
+    
+})
+
+server.put('/api/users/:id', (req, res) => {
+    
+})
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
